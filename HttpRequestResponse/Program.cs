83570aaa4d4a78f82cfe2e0e0e6d5e6b1c9c4abe
Per A. Bergersen <pab2222@hotmail.com>
2024-10-29 14:51:12 +0100

    Add project files.

---
+++ HttpRequestResponse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class HttpExample
+{
+    static async Task Main(string[] args)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                // Send a GET request
+                HttpResponseMessage response = await client.GetAsync("https://www.example.com");
+                response.EnsureSuccessStatusCode();
+
+                // Read response content
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("Response Body:");
+                Console.WriteLine(responseBody);
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message: {0} ", e.Message);
+            }
+        }
+        Console.ReadLine();
+    }
+}
